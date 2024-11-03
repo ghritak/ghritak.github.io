883acd210d323dd95a3f46c6abefd6913483b152
ghritak <ghritakjyotikalita@gmail.com>
2024-11-03 22:16:55 +0530

    feature: ui changes

--- src/components/nav/NavbarComponent.js
+++ src/components/nav/NavbarComponent.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import NavbarButton from './NavbarButton';
 import { scrollToSection } from '../../utils/scroll';
 
 const NavbarComponent = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    if (window.scrollY > 50 && !isScrolled) {
+      console.log('scrolled');
+      setIsScrolled(true);
+    } else if (window.scrollY <= 50 && isScrolled) {
+      console.log('not scrolled');
+      setIsScrolled(false);
+    }
+  }, [isScrolled]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
-    <div className='fixed bottom-4 xl:bottom-auto xl:-top-4 sm:pt-10 rounded-bl-custom rounded-br-custom z-50 xl:backdrop-blur-sm '>
+    <div className='fixed bottom-4 xl:bottom-auto xl:-top-4 sm:pt-10 rounded-bl-custom rounded-br-custom z-50'>
       <header className='flex justify-center items-center w-screen'>
-        <div className='flex flex-row justify-around w-full max-w-sm mx-5 xl:max-w-2xl bg-zinc-900 xl:bg-zinc-800 rounded-full py-3 xl:py-4'>
+        <div
+          className={`flex flex-row justify-around w-full max-w-sm mx-5 xl:max-w-2xl transition-all duration-300 ${
+            isScrolled ? 'bg-zinc-900' : 'bg-zinc-900 xl:bg-zinc-800'
+          } rounded-full py-3 xl:py-4`}
+        >
           <NavbarButton onClick={() => scrollToSection('home')}>
             Home
           </NavbarButton>
